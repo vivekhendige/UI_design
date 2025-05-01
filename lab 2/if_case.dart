10d2acc6772d2d70fa6bf7f996a3df1551e3cf0a
vivekhendige <vivekhendige03@gmail.com>
2025-05-01 23:24:50 +0530

    Create if_case.dart

---
+++ lab 2/if_case.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+void main(){
+	stdout.write("Enter val-1:");
+	int a=int.parse(stdin.readLineSync()!);
+	stdout.write("Enter val-2:");
+	int b=int.parse(stdin.readLineSync()!);
+	if(a case b){
+		print('Val1 is in val2');
+}
+}
